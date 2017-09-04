b350fc1ef2bd9f0002e0cc9082aa2ed7aeccb64e
YuraBobkov <yura_@tut.by>
2017-09-05 01:27:08 +0300

    create combineReducers for cities and weather

--- js/store.js
+++ js/store.js
@@ -3,13 +3,19 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 
+import  request  from './middleware';
+import  citiesReducer  from './reducers/citiesReducer';
+import  weatherReducer  from './reducers/weatherReducer';
 
 const store = createStore(combineReducers({
+  cities: citiesReducer,
+  weather: weatherReducer,
 }),
   applyMiddleware(
     promise(),
     thunk,
     logger(),
+    request
   ));
 
 export default store;
