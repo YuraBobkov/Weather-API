ecdeadb0aa2f90ba876e4434a4eb1e45202dbd75
YuraBobkov <yura_@tut.by>
2017-09-05 01:30:44 +0300

    create submit event for Input

--- js/components/Input.js
+++ js/components/Input.js
@@ -3,6 +3,12 @@ import React from 'react';
 export default class Input extends React.Component {
   constructor(props) {
     super(props);
+    this.onKeyPress = this.onKeyPress.bind(this);
+  }
+  onKeyPress(e) {
+    if (e.nativeEvent.keyCode === 13) {
+      this.props.getCities(e.target.value);
+    }
   }
   render() {
     return (
